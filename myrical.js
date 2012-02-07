f38eee14a6f2225627541f2ccc63eb94e4229397
Simon Coltman <simon.coltman@gmail.com>
2012-02-07 14:11:56 +0000

    add question words

--- myrical.js
+++ myrical.js
@@ -1,10 +1,6 @@
-var Myrical = (function() {
+var Myrical = (function() {  
   var filesRead = 0,
-      lastFile = false,
-      wordCount = 0,
-      uniqueCount = 0;
-  
-  
+      words = [];
   /**
    * Initialise.
    */
@@ -17,6 +13,12 @@ var Myrical = (function() {
    */  
   var handleFileSelect = function(evt) {
     var files = evt.target.files;
+    var fileWords = [],
+        lastFile = false;
+    
+    words = [];
+    filesRead = 0;
+    
     for (var i = 0, f; f = files[i]; i++) {
       readBlob(f, files.length);
     }
@@ -26,16 +28,15 @@ var Myrical = (function() {
    * Slices blobs to create a string of file content
    */
   var readBlob = function(file, flength) {
-    var fileWords = [],
-        reader = null,
+    var reader = null,
         blob = null;
-    
+        
     if (!file) {
        alert('Please select a file!');
        return;
     }
     reader = new FileReader();
-   
+    
     if (file.webkitSlice) {
       blob = file.webkitSlice(0, file.size);
     } else if (file.mozSlice) {
@@ -43,18 +44,25 @@ var Myrical = (function() {
     } else if (file.slice) {
       blob = file.slice(0, file.size);
     }
-   
+    
     reader.readAsBinaryString(blob);
-   
+    
     reader.onloadend = function(evt) {
+      
       filesRead++;
+      
       if (filesRead == flength) {
         lastFile = true;
       }
       if (evt.target.readyState == FileReader.DONE) {
         var fileContents = evt.target.result;
-        words = processText(fileContents);
-        wordCount = words.length;
+        
+        fileContents = fileContents.toLowerCase();
+        
+        fileWords = fileContents.match(/\b[A-Za-z']+\b/g);
+        for (var i = 0, j = fileWords.length; i < j; i++) {
+          words.push(fileWords[i]);
+        }
         
         if (lastFile) {
           processWords(words);
@@ -64,21 +72,6 @@ var Myrical = (function() {
   }
   
   /**
-   * Match individual words in file content.
-   */  
-  var processText = function(fileContents){
-    var words = [],
-        fileWords = [];
-    fileWords = fileContents.match(/\b[A-Za-z']+\b/g);
-    for (var i = 0, j = fileWords.length, word; i < j; i++) {
-      words.push(fileWords[i]);
-    }
-    
-    return words;
-  }
-  
-  
-  /**
    * Create object of 'word' to 'frequency' relationship.
    */
   var processWords = function(words){
@@ -86,6 +79,8 @@ var Myrical = (function() {
         sameWord = '',
         word = '',
         countedWords = {};
+        uniqueCount = 0,
+        wordCount = words.length;
         
     for (var i = 0; i < words.length; i++) {
       word = words[i];
@@ -101,7 +96,7 @@ var Myrical = (function() {
       }
     }
     
-    render(countedWords);
+    render(countedWords, wordCount, uniqueCount);
   }
   
   /**
@@ -112,7 +107,7 @@ var Myrical = (function() {
         pt = 0,
         ct = 0,
         types = {
-          "you": ['I','me','my','mine'],
+          "you": ['i','me','my','mine'],
           "someone else": ["you","you're", "yours"],
           "her": ['her','she','hers'],
           "him": ['him','his','he'],
@@ -142,7 +137,7 @@ var Myrical = (function() {
   /**
    * Render words object to browser.
    */
-  var render = function(countedWords){
+  var render = function(countedWords, wordCount, uniqueCount){
     var percentUnique = Math.round((uniqueCount/wordCount) * 100),
         restNum = 100 - percentUnique,
         chart = '<img class="chart-unique" src="http://chart.apis.google.com/chart?chs=293x205&cht=p&chco=3399CC&chds=0,98.333&chd=t:'+percentUnique+','+restNum+'">',
@@ -151,14 +146,27 @@ var Myrical = (function() {
         subjectType = getType(countedWords),
         allAbout = '<p class="all-about">It\'s all about '+subjectType+'.</p>',
         sortWords = [],
-        threeUnique = 0;
-    
+        threeUnique = 0,
+        mostQuestion = 1,
+        questionWords = {
+          'what':0,
+          'why':0,
+          'when':0,
+          'where':0,
+          'how':0,
+          'who':0
+        };
+        
     // create array of words and sort
-    for (var k in countedWords) {
-      sortWords.push(countedWords[k]);
+    for (var key in countedWords) {
+      if(key in questionWords){
+        questionWords[key] = {};
+        questionWords[key].count = countedWords[key].count;
+        if(countedWords[key].count > mostQuestion) { mostQuestion = countedWords[key].count; };
+      }
+      sortWords.push(countedWords[key]);
     }
     sortWords.sort(compareLength);
-    
     
     for (var i = 0; i < sortWords.length; i++) {
       
@@ -175,8 +183,34 @@ var Myrical = (function() {
     }  
     uniqueExamples = uniqueExamples + '</p>';
     
-    $('#content').html(uniqueWords + uniqueExamples + allAbout);
+    var questionText = '',
+        questionList = '',
+        hasQuestions = false,
+        perdec = 1;
+      
+    var minLength = (mostQuestion + '').length;
+
+    for (key in questionWords) {
+      if (questionWords[key].count > 0) {
+        hasQuestions = true;
+        perdec = (Math.floor((questionWords[key].count / mostQuestion) * 10));
+        if (perdec < minLength) { perdec = minLength }
+
+        questionWidth = 21 * perdec;
+        questionList = questionList + '<li><span style="width:'+questionWidth+'px">' + questionWords[key].count + " </span>" + key + "</li>";
+      }
+    }
+    
+    if (hasQuestions) {
+      questionText = '<ul class="questions">' + questionList + '</ul>';
+    }
+    $('#content').html(questionText + uniqueWords + uniqueExamples + allAbout);
   
+  }
+  
+  
+  var calculateBarWidth = function(){
+    
   }
   
   var compareCount = function(a, b) {
